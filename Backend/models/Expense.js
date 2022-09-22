96d2bf4b2a2764c74ed2ee68fb79aa5620a2e688
nandweza <allannandweza@gmail.com>
2022-09-22 16:19:22 +0300

    updates

--- Backend/models/Expense.js
+++ Backend/models/Expense.js
@@ -5,10 +5,12 @@ const ExpenseSchema = new mongoose.Schema(
     id: {type: Number, required: true, unique: true},
     item: {type: String, required: true},
     quantity: {type: Number, required: true},
-    _cost: {type: Number, required: true},
+    cost: {type: Number, required: true},
     price: {type: Number, required: true},
     week: {type: Number, required: true},
-    _month: {type: String, required: true},
+    month: {type: String, required: true},
+    year: {type: String, required: true},
+    isMonthly: {type: Boolean, default: false},
   },
   { timestamps: true }
 );
--- Backend/models/ExpenseList.js
+++ Backend/models/ExpenseList.js
@@ -2,10 +2,9 @@ const mongoose = require("mongoose");
 
 const ExpenseListSchema = new mongoose.Schema(
   {
-    title: {type: String, required: true},
-    quantity: {type: Number, required: true},
-    week: {type: Array},
-    _month: {type: Array},
+    title: {type: String, required: true, unique: true},
+    type: {type: String},
+    content: {type: Array},
   },
   { timestamps: true }
 );
